45d0aba867af16e621a20590e06883b0c55645b6
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-01 19:41:53 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -8,12 +8,13 @@ from time import time
 from datetime import datetime
 from modules.helpers.filters import command
 from modules.helpers.command import commandpro
-from pyrogram import Client, filters
+from pyrogram import Client, filters, __version__ as pyrover
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ChatJoinRequest
 from image import START_IMG_URL, BOT_USERNAME, GROUP_SUPPORT, BOT_NAME
 import random
 from pyrogram.errors import UserNotParticipant
 from plugins import __version__
+
 from modules.__main__ import bot
 START_TIME = datetime.utcnow()
 START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
