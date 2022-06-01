f6b948d99ef42fe32d87073d727ce48a85b8ae3a
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-01 18:35:06 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -12,6 +12,25 @@ bot = Bot(
     plugins=dict(root="plugins")
 )
 
+
+santhu = Client(
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_name=SESSION_NAME,
+    
+    )
+
+user = PyTgCalls(santhu,
+    cache_duration=100,
+    overload_quiet_mode=True,)
+
+call_py = PyTgCalls(santhu, overload_quiet_mode=True)
+
+with Client(":santhu:", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+    me_bot = app.get_me()
+with santhu as app:
+    me_santhu = app.get_me()
+
 bot.start()
 run()
 idle()
