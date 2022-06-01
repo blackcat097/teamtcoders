33199a269ca7a86eb9ef1da3742f1f7c55b5f81e
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-01 15:41:40 +0530

    Update image.py

--- image.py
+++ image.py
@@ -3,7 +3,7 @@ from os import getenv
 from dotenv import load_dotenv
 
 
-
+BOT_USERNAME = getenv("BOT_USERNAME") 
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "tgshadow_fighters")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "teamshadowprojects")
 
