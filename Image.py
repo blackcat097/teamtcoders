c40a42befceb7e824f017c199845d67cf43a4b84
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-01 14:31:32 +0530

    Create Image.py

---
+++ Image.py
@@ -0,0 +1,25 @@
+## BOT_IMAGE
+
+START_IMG_URL = [
+ "https://te.legra.ph/file/bd8bc8134550113f1bc7d.jpg", 
+ "https://te.legra.ph/file/e709c376815fcdb9e7689.jpg", 
+ "https://te.legra.ph/file/ae5c5aaac8f73987255c3.jpg", 
+ "https://te.legra.ph/file/9eef3af2b79fc94beb863.jpg", 
+ "https://te.legra.ph/file/e8b999e4f598d4306c34b.jpg", 
+ "https://te.legra.ph/file/5d0b393e0a620db6f9792.jpg", 
+ "https://te.legra.ph/file/67b6c7f92d83e36f131ab.jpg", 
+ "https://te.legra.ph/file/f3deab49dbce180d1a992.jpg", 
+ "https://te.legra.ph/file/953874b219182df501613.jpg", 
+ "https://telegra.ph/file/c2ff65c5f7ddbb6a52170.jpg", 
+ "https://te.legra.ph/file/449a5c8301dc0f48f842e.jpg", 
+ "https://te.legra.ph/file/39e707ee27325867e27b1.jpg", 
+ "https://te.legra.ph/file/90812b7fa0538389943ab.jpg", 
+ "https://te.legra.ph/file/914fc426bed31dbe4bdc2.jpg", 
+ "https://te.legra.ph/file/28f95b221efbefede9988.jpg", 
+ "https://telegra.ph/file/290e055a47df326b6e908.jpg", 
+ "https://telegra.ph/file/4a2c348dc10f4799ecc23.jpg", 
+ "https://telegra.ph/file/e5d14734cc62fb45ebb80.jpg", 
+ "https://telegra.ph/file/52771fab9aa447154ecfd.jpg", 
+ "https://telegra.ph/file/28af6e4d2f6c9849ccd8e.jpg", 
+ "https://telegra.ph/file/de94973a185e70e207363.jpg"
+]
