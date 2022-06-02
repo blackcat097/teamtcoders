bf6da7d12b55729bfc7d1ad7425e98db2eaf956c
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-02 09:27:45 +0530

    Update decorators.py

--- modules/helpers/decorators.py
+++ modules/helpers/decorators.py
@@ -71,17 +71,3 @@ def check_blacklist():
         return wrapper
 
     return decorator
-
-
-def authorized_users_only(func: Callable) -> Callable:
-    async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
-            return await func(client, message)
-
-        administrators = await get_administrators(message.chat)
-
-        for administrator in administrators:
-            if administrator == message.from_user.id:
-                return await func(client, message)
-
-    return decorator
