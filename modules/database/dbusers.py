ec589613e0dca0e05b4c874bcf837fec1af5913e
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-02 16:12:00 +0530

    Update dbusers.py

--- modules/database/dbusers.py
+++ modules/database/dbusers.py
@@ -1,6 +1,10 @@
 import motor.motor_asyncio
 
-
+class Database:
+    def __init__(self, url, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(url)
+        self.db = self._client[database_name]
+        self.col = self.db.users
 
 async def total_users_count(self):
         count = await self.col.count_documents({})
