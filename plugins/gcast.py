2b5e35ecc6342b4d009b6d2c85f381e6dc06dc51
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-02 09:44:03 +0530

    Update gcast.py

--- plugins/gcast.py
+++ plugins/gcast.py
@@ -1,14 +1,14 @@
-# Copyright (C) 2021 By AdityaPlayer
+
 
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
-from modules.clientbot.clientbot import client as aditya
+from modules.clientbot.clientbot import client as shadow
 from modules.config import SUDO_USERS
 
-@Client.on_message(filters.command(["gcast", "post", "send"]))
-async def broadcast(_, message: Message):
+@Client.on_message(filters.command(["bcast"]))
+async def bcast(_, message: Message):
     sent=0
     failed=0
     if message.from_user.id not in SUDO_USERS:
@@ -21,7 +21,7 @@ async def broadcast(_, message: Message):
         lmao = message.reply_to_message.text
         async for dialog in aditya.iter_dialogs():
             try:
-                await aditya.send_message(dialog.chat.id, lmao)
+                await shadow.send_message(dialog.chat.id, lmao)
                 sent = sent+1
                 await wtf.edit(f"`ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ` \n\n**sᴇɴᴛ ᴛᴏ:** `{sent}` ᴄʜᴀᴛs \n**ғᴀɪʟᴇᴅ ɪŋ:** {failed} ᴄʜᴀᴛs")
                 await asyncio.sleep(3)
