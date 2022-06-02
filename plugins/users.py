eb9da2336b88f54b9ac48050fd26be7e23b79d25
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-02 15:55:18 +0530

    Update users.py

--- plugins/users.py
+++ plugins/users.py
@@ -1,1 +1,16 @@
+from modules.database.import dbusers
+
+
+@Bot.on_message(filters.private & filters.command("status"), group=5)
+async def status(bot, update):
+
+    total_users = await db.total_users_count()
+    text = "**ʙᴏᴛ sᴛᴀᴛᴜs**\n"
+    text += f"\n**ᴛᴏᴛᴀʟ ᴜsᴇʀs:** `{total_users}`"
+
+    await update.reply_text(
+        text=text,
+        quote=True,
+        disable_web_page_preview=True
+    )
 
