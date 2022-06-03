8ebcc4c5bc60ecde2bc21c8913e85c6a2e1fdd15
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-03 22:15:38 +0530

    Update decorators.py

--- modules/helpers/decorators.py
+++ modules/helpers/decorators.py
@@ -49,6 +49,20 @@ def bot_creator(func: Callable) -> Callable:
     return decorator
 
 
+def humanbytes(size):
+    """Convert Bytes To Bytes So That Human Can Read It"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
 def check_blacklist():
     def decorator(func):
         @wraps(func)
