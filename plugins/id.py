779ce3c4810a9134effef2ddda09fefcd4fa6950
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-06-03 22:58:08 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -15,3 +15,15 @@ async def id(c: Client, message: Message):
             message.chat.id
         ), 
     ) 
+
+
+@Client.on_callback_query(filters.regex("id"))
+async def id(_, query: CallbackQuery):
+    await query.answer("chat id")
+    await query.edit_message_text(
+        f"""👋🏻 **ʜᴇʟʟᴏ [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**
+➠ **/id ᴛʏᴘᴇ ɪᴅ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ ᴀʀᴇ ᴘᴇʀsᴏɴᴀʟ !**""", 
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("•◁•", callback_data="command_list")]]
+        ),
+    )
